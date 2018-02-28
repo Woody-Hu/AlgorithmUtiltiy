9c0701edf39718bfa030f0d2fb1ecce3727dc3c9
Woody-Hu <812791840@qq.com>
2018-02-28 17:29:59 +0800

    Initial commit

---
+++ RBTree/RBTreeClass.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBTree
+{
+    public class RBTreeClass<T>
+        where T : IComparable
+    {
+        private RBTreeNode<T> m_thisRoot = null;
+
+        internal RBTreeNode<T> RootNode
+        {
+            get
+            {
+                return m_thisRoot;
+            }
+
+            set
+            {
+                m_thisRoot = value;
+            }
+        }
+
+        public bool Find(T inputValue)
+        {
+            if (InputCheck(inputValue))
+            {
+                return false;
+            }
+
+            var findedNode = FindLoop(RootNode, inputValue);
+
+            return (null != findedNode && !findedNode.IfIsRemoved);
+        }
+
+        private bool InputCheck(T inputValue)
+        {
+            return null == m_thisRoot || null == inputValue;
+        }
+
+        private RBTreeNode<T> FindLoop(RBTreeNode<T> nowNode,T inputValue)
+        {
+            if (null == nowNode)
+            {
+                return null;
+            }
+
+            var compareValue = inputValue.CompareTo(nowNode.ThisValue);
+
+            if (compareValue == 0)
+            {
+                return nowNode;
+            }
+            else if (compareValue > 0)
+            {
+                return FindLoop(nowNode.RightNode, inputValue);
+            }
+            else
+            {
+                return FindLoop(nowNode.LeftNode, inputValue);
+            }
+        }
+
+        public bool Insert(T inputValue)
+        {
+            if (null == inputValue)
+            {
+                return false;
+            }
+
+            if (null == RootNode)
+            {
+                RootNode = new RBTreeNode<T>(inputValue);
+                return true;
+            }
+
+            var findedValue = FindLoop(RootNode, inputValue);
+
+            if (null != findedValue)
+            {
+                if (findedValue.IfIsRemoved)
+                {
+                    findedValue.ThisValue = inputValue;
+                    findedValue.IfIsRemoved = false;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T inputValue)
+        {
+            if (InputCheck(inputValue))
+            {
+                return false;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RBTree/RBTreeNode.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBTree
+{
+    public class RBTreeNode<T>
+        where T:IComparable
+    {
+        T m_thisValue;
+
+        bool m_bIfIsRemoved;
+
+        bool m_bIfIsRed;
+
+        RBTreeNode<T> m_leftNode;
+
+        RBTreeNode<T> m_rightNode;
+
+        RBTreeNode<T> m_parent;
+
+        public T ThisValue
+        {
+            get
+            {
+                return m_thisValue;
+            }
+
+            internal set
+            {
+                m_thisValue = value;
+            }
+        }
+
+        public bool IfIsRed
+        {
+            get
+            {
+                return m_bIfIsRed;
+            }
+
+            internal set
+            {
+                m_bIfIsRed = value;
+            }
+        }
+
+        public RBTreeNode<T> LeftNode
+        {
+            get
+            {
+                return m_leftNode;
+            }
+
+            internal set
+            {
+                m_leftNode = value;
+            }
+        }
+
+        public RBTreeNode<T> RightNode
+        {
+            get
+            {
+                return m_rightNode;
+            }
+
+            internal set
+            {
+                m_rightNode = value;
+            }
+        }
+
+        public RBTreeNode<T> Parent
+        {
+            get
+            {
+                return m_parent;
+            }
+
+            internal set
+            {
+                m_parent = value;
+            }
+        }
+
+        public bool IfIsRemoved
+        {
+            get
+            {
+                return m_bIfIsRemoved;
+            }
+
+            internal set
+            {
+                m_bIfIsRemoved = value;
+            }
+        }
+
+        public RBTreeNode(T inputValue,bool ifIsRed = true)
+        {
+            ThisValue = inputValue;
+            m_bIfIsRed = ifIsRed;
+            m_bIfIsRemoved = false;
+        }
+
+        internal void LeftRoate(RBTreeClass<T> useTree)
+        {
+            var thisRightValue = this.RightNode;
+
+            var thisRigthLeft = this.RightNode.LeftNode;
+
+            this.RightNode = thisRigthLeft;
+
+            if (null != this.RightNode)
+            {
+                this.RightNode.Parent = this;
+            }
+
+            thisRightValue.Parent = this.Parent;
+
+            if (this.Parent.LeftNode == this)
+            {
+                this.Parent.LeftNode = thisRigthLeft;
+            }
+            else
+            {
+                this.Parent.RightNode = thisRigthLeft;
+            }
+
+            thisRightValue.LeftNode = this;
+
+            this.Parent = thisRightValue;
+
+            if (this == useTree.RootNode)
+            {
+                useTree.RootNode = this.Parent;
+            }
+        }
+
+        internal void RightRoate(RBTreeClass<T> useTree)
+        {
+            var thisLeftValue = this.LeftNode;
+
+            var thisLeftRightValue = thisLeftValue.RightNode;
+
+            this.LeftNode = thisLeftRightValue;
+
+            if (null != this.LeftNode)
+            {
+                this.LeftNode.Parent = this;
+            }
+
+            thisLeftValue.Parent = this.Parent;
+
+            if (this.Parent.LeftNode == this)
+            {
+                this.Parent.LeftNode = thisLeftValue;
+            }
+            else
+            {
+                this.Parent.RightNode = thisLeftValue;
+            }
+
+            thisLeftValue.RightNode = this;
+
+            this.Parent = thisLeftValue;
+
+            if (this == useTree.RootNode)
+            {
+                useTree.RootNode = this.Parent;
+            }
+
+        }
+        
+    }
+}
